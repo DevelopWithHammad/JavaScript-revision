eb0cf6d07ade87ed721450e34beb7bc27206b333
Hammad Ur Rehman <141633644+DevelopWithHammad@users.noreply.github.com>
2023-08-29 14:20:15 +0500

    differences

---
+++ differences/app.js
@@ -0,0 +1,7 @@
+// // // Difference between === and ==
+
+// const num1 = 5
+// const num2 = "5"
+
+// console.log(num1 == num2);  // Check only values
+// console.log(num1 === num2); // Check values and data type
