0f91c2bc681976825705cef8dd552574582f0589
Laraib007 <lrbkhan01@gmail.com>
2023-09-13 11:33:39 +0500

    Date Methods

---
+++ Date Methods/app.js
@@ -0,0 +1,111 @@
+// // Creating Date
+// let nowDate = new Date;
+// console.log("Today is: ", nowDate)
+
+
+// // toDateString
+// // It only shows the weekDay, Month, and year 
+
+// let nowDate = new Date;
+// let nowDateString = nowDate.toDateString();  // it returns back into a new variable
+// console.log("Today is: ",nowDateString)
+
+
+
+
+// // getDate
+// // It only shows today's date
+// let nowDate = new Date;
+// let nowDateOnly = nowDate.getDate();  // it returns back into a new variable
+// console.log("Today's date is: ",nowDateOnly)
+
+
+
+
+// // getFullYear
+// // It only shows current year
+// let nowDate = new Date;
+// let nowDateYearOnly = nowDate.getFullYear();  // it returns back into a new variable
+// console.log("Current year is : ",nowDateYearOnly)
+
+
+
+
+// // getMonth
+// // It only shows current month
+// let nowDate = new Date;
+// let nowDateMonthOnly = nowDate.getMonth();  // it returns back into a new variable
+// console.log("Current month is : ", nowDateMonthOnly)
+
+
+
+
+// // getDay
+// // It only shows current day
+// let Day = new Date;
+// let currentDay = Day.getDay();  // it returns back into a new variable
+// console.log("Current day is : ", currentDay)
+
+
+
+
+
+// // getHours
+// // It only shows current hour
+// let hour = new Date;
+// let currentHour = hour.getDay();  // it returns back into a new variable
+// console.log("Current hour is: ", currentHour)
+
+
+
+
+// // getMinutes
+// // It only shows current minute
+// let minutes = new Date;
+// let currentminutes = minutes.getMinutes();  // it returns back into a new variable
+// console.log("Current minute is: ", currentminutes)
+
+
+
+// // getSecond
+// // It only shows current second
+// let second = new Date;
+// let currentSecond = second.getSeconds();  // it returns back into a new variable
+// console.log("Current second is: ", currentSecond)
+
+
+
+
+// // getMilliSecond
+// // It only shows current millisecond
+// let mSecond = new Date;
+// let currentMilliSecond = mSecond.getMilliseconds();  // it returns back into a new variable
+// console.log("Current milli second is: ", currentMilliSecond)
+
+
+
+
+
+
+
+
+
+
+
+
+// // ======= SET DATE METHODS =======
+
+// // setting future date
+// let now = new Date;
+//  now.setDate(17); // it shows the future details of 17th day of current month and year
+// console.log(now);
+
+
+
+// // setting future date
+// let now = new Date;
+// now.setFullYear(2022); // it shows the future details of 17th day of current month and year
+// console.log(now);
+
+
+
