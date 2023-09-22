62dc85a3a16663928649820586c2899e34ac4579
Asad Rao <asad.rao@teksyo.com>
2023-09-22 20:49:50 +0500

    number and math

---
+++ Numbers and Date/app.js
@@ -0,0 +1,21 @@
+// const num = new Number(100);
+// console.log("num =====>>>>>>", num);
+
+// const numInString = num.toString();
+// console.log("num in string =====>>>>>", numInString);
+
+
+// const num = 13.999;
+// console.log("num====>>>>", num);
+// console.log("Precision======>>>>>", num.toPrecision(2));
+// console.log("toFixed=======>>>>>>>>", num.toFixed(6));
+
+
+
+// number======================================
+
+const num = Math.random();
+console.log("math.random======>>>>>>", num);
+const roundNum = num.Math.round()
+console.log(roundNum    );
+
--- strings in javascript/app.js
+++ strings in javascript/app.js
@@ -1,22 +1,36 @@
-const msg = "Hello world!" // simple string
-console.log(msg); 
-console.log("datatype=====>>>", typeof msg);
-console.log("substring======>>>>>>", msg.substring(0, 5)) // only hello will be display
-console.log("chatAt=====>>>>", msg.charAt(0)) // identify what charater is in the 0 position
-console.log("rouppercase=====>>>>", msg.toUpperCase()) // convert it to uppercase
-console.log("tolowercase=====>>>>", msg.toLowerCase()) // convert it to lowercase
-console.log("replace=====>>>>", msg.replace("world", "universe")) // convert world to universe
-console.log("inludes=====>>>>", msg.includes("Hello")) // tell that is Hello includes or not
-console.log("indexOf=====>>>>", msg.indexOf("H")) // tell the index number of H
+// const msg = "Hello world!" // simple string
+// console.log(msg); 
+// console.log("datatype=====>>>", typeof msg);
+// console.log("substring======>>>>>>", msg.substring(0, 5)) // only hello will be display
+// console.log("chatAt=====>>>>", msg.charAt(0)) // identify what charater is in the 0 position
+// console.log("rouppercase=====>>>>", msg.toUpperCase()) // convert it to uppercase
+// console.log("tolowercase=====>>>>", msg.toLowerCase()) // convert it to lowercase
+// console.log("replace=====>>>>", msg.replace("world", "universe")) // convert world to universe
+// console.log("inludes=====>>>>", msg.includes("Hello")) // tell that is Hello includes or not
+// console.log("indexOf=====>>>>", msg.indexOf("H")) // tell the index number of H
 
 
-const trim = "      Hammad      "
-console.log("original string====>>>>", trim);
-console.log("trim====>>>>", trim.trim());
+// const trim = "      Hammad      "
+// console.log("original string====>>>>", trim);
+// console.log("trim====>>>>", trim.trim());
 
-const str = new String("Hammad") // it creates an object
-console.log(str); 
-console.log("datatype=====>>>>>>>", typeof str); 
-console.log(`${str[0]}${str[1]}${str[2]}${str[3]}${str[4]}${str[5]}`);
+// const str = new String("Hammad") // it creates an object
+// console.log(str); 
+// console.log("datatype=====>>>>>>>", typeof str); 
+// console.log(`${str[0]}${str[1]}${str[2]}${str[3]}${str[4]}${str[5]}`);
+
+// const str = "Hammad Ur Rehman";
+// console.log("name========>>>>>>", str);
+
+// const splitStr =  str.split(" ")
+// console.log("split str======>>>>>>>", splitStr);
+// console.log(`again str======>>>>>> ${splitStr[0]} ${splitStr[1]} ${splitStr[2]}`) ;
 
 
+// const splitStr =  str.split(",")
+// console.log("split str======>>>>>>>", splitStr);
+// console.log(`again str======>>>>>> ${splitStr[0]} ${splitStr[1]} ${splitStr[2]}`) ;
+
+// const myName = "Hammad Ur Rehman";
+// console.log("fullname=====>>>>>", myName)
+// console.log("first name=====>>>>>", myName.substring(0, 7))
